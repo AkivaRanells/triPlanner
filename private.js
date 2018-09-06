7ca197159d8de354068f10a65c6edfb14337c255
Akiva Ranells <akranells@gmail.com>
2018-09-06 10:51:14 +0300

    added keys

---
+++ private.js
@@ -0,0 +1,5 @@
+const HERE_APP_ID = "fZM1xfZJOQY5V2rEYc0T";
+const HERE_APP_CODE="O1FhUetLz5hd7ndb2Ru_FA";
+
+module.exports.HERE_APP_ID = HERE_APP_ID;
+module.exports.HERE_APP_CODE = HERE_APP_CODE;
